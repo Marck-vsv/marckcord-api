4d8fcef17d4c220bb6e37065ae6c7ecb50609b68
Marckvsv <vini.marcksv@outlook.com>
2025-01-27 08:19:17 -0300

    chore: update user model/dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,14 +1,40 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Length,
+} from 'class-validator';
 
 export class CreateUserDto {
     @IsNotEmpty({ message: 'The email cannot be empty.' })
     @IsString({ message: 'The email must be of string type.' })
     @IsEmail({}, { message: 'Invalid email.' })
+    @Length(5, 255, {
+        message: 'The email must be between 5 and 255 characters.',
+    })
     email: string;
 
     @IsNotEmpty({ message: 'The username cannot be empty.' })
     @IsString({ message: 'The username must be of string type.' })
+    @Length(5, 36, {
+        message: 'The username must be between 5 and 36 characters.',
+    })
     username: string;
+
+    @IsString({ message: 'The display name must be of string type.' })
+    @Length(5, 36, {
+        message: 'The display name must be between 5 and 36 characters.',
+    })
+    @IsOptional()
+    displayName: string | null;
+
+    @IsString({ message: 'The bio must be of string type.' })
+    @Length(1, 500, {
+        message: 'The bio must be between 1 and 500 characters.',
+    })
+    @IsOptional()
+    bio: string | null;
 
     @IsNotEmpty({ message: 'The password cannot be empty.' })
     @IsString({ message: 'The password must be of string type.' })
