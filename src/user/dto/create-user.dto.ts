eb3aa299c3148e4808a188d69dae2aa4f32d6aac
Marckvsv <vini.marcksv@outlook.com>
2025-02-05 11:54:29 -0300

    refact: change messages

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -11,30 +11,30 @@ export class CreateUserDto {
     @IsString({ message: 'The email must be of string type.' })
     @IsEmail({}, { message: 'Invalid email.' })
     @Length(5, 255, {
-        message: 'The email must be between 5 and 255 characters.',
+        message: 'The email must have between 5 and 255 characters.',
     })
     email: string;
 
     @IsNotEmpty({ message: 'The username cannot be empty.' })
     @IsString({ message: 'The username must be of string type.' })
     @Length(5, 36, {
-        message: 'The username must be between 5 and 36 characters.',
+        message: 'The username must have between 5 and 36 characters.',
     })
     username: string;
 
     @IsString({ message: 'The display name must be of string type.' })
-    @Length(5, 36, {
-        message: 'The display name must be between 5 and 36 characters.',
+    @Length(0, 36, {
+        message: 'The display name cannot have more than 36 characters.',
     })
     @IsOptional()
-    displayName: string | null;
+    displayname: string;
 
     @IsString({ message: 'The bio must be of string type.' })
-    @Length(1, 500, {
-        message: 'The bio must be between 1 and 500 characters.',
+    @Length(0, 512, {
+        message: 'The bio cant have more than 512 characters.',
     })
     @IsOptional()
-    bio: string | null;
+    bio: string;
 
     @IsNotEmpty({ message: 'The password cannot be empty.' })
     @IsString({ message: 'The password must be of string type.' })
