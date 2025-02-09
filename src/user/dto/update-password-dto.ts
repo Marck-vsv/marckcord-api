058a047faabc932d304a25a0396476bb2b5df4f4
Marckvsv <vini.marcksv@outlook.com>
2025-02-09 00:20:34 -0300

    chore: change email message and remove unecessary @IsString decorator

--- src/user/dto/update-password-dto.ts
+++ src/user/dto/update-password-dto.ts
@@ -1,9 +1,8 @@
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class UpdatePasswordDto {
-    @IsString({ message: 'The username must be of string type.' })
     @IsEmail({}, { message: 'Invalid email.' })
-    @IsNotEmpty({ message: 'The username cannot be empty.' })
+    @IsNotEmpty({ message: 'The email cannot be empty.' })
     @Length(5, 255, {
         message: 'The email must have between 5 and 255 characters.',
     })
