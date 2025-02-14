ae04a16fac0d71c73b67c77fa007d7d2cb3f23ad
Marckvsv <vini.marcksv@outlook.com>
2025-02-14 16:08:20 -0300

    feat: add cors exception

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(process.env.PORT ?? 6666);
+    app.enableCors({
+        allowedHeaders: ['content-type'],
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+    await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
